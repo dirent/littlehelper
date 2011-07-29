3d67094a6fa998b49e67db2aa4019c9dcc19370c
dirk <dirk@0d842271-ca0e-0410-b7f0-83f617a2c6fd>
2011-07-29 21:27:22 +0000

    Kreditrechner-Form mit Tapestry 5 realisiert
    git-svn-id: http://wikiserver/svn/littlehelper/trunk@666 0d842271-ca0e-0410-b7f0-83f617a2c6fd

--- src/main/java/de/dirent/littlehelper/pages/Kreditrechner.java
+++ src/main/java/de/dirent/littlehelper/pages/Kreditrechner.java
@@ -1,14 +1,44 @@
 package de.dirent.littlehelper.pages;
 
 
+import org.apache.tapestry5.PersistenceConstants;
 import org.apache.tapestry5.annotations.Import;
 import org.apache.tapestry5.annotations.Persist;
 import org.apache.tapestry5.annotations.Property;
+import org.apache.tapestry5.annotations.SetupRender;
 
 
 @Import( stylesheet="context:/css/littlehelper.css" )
 public class Kreditrechner {
 
 	@Property @Persist
+	private boolean startKreditrechner;
+	
+	@Property @Persist
+	private Double betrag;
+	
+	@Property @Persist
+	private Double zins;
+	
+	@Property @Persist
+	private Double tilgung;
+	
+	@Property @Persist
 	private boolean showResult;
+	
+	@Property @Persist( PersistenceConstants.FLASH )
+	private String errorMessage;
+	
+	@SetupRender
+	public void init() {
+		
+		if( betrag == null ) betrag = 10000.0;
+		if( zins == null ) zins = 0.05;
+		if( tilgung == null ) tilgung = 0.01; 
+	}
+	
+	public void onSuccessFromCalculate() {
+		
+		startKreditrechner = true;
+	}
 }
