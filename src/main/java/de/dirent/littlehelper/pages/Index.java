38be894eda32b9476978f3804ad211997dbab101
dirk <dirk@0d842271-ca0e-0410-b7f0-83f617a2c6fd>
2011-08-17 19:59:20 +0000

    IP-Detektor
    git-svn-id: http://wikiserver/svn/littlehelper/trunk@676 0d842271-ca0e-0410-b7f0-83f617a2c6fd

--- src/main/java/de/dirent/littlehelper/pages/Index.java
+++ src/main/java/de/dirent/littlehelper/pages/Index.java
@@ -1,9 +1,21 @@
 package de.dirent.littlehelper.pages;
 
 
+import javax.servlet.http.HttpServletRequest;
+
 import org.apache.tapestry5.annotations.Import;
+import org.apache.tapestry5.ioc.annotations.Inject;
+import org.apache.tapestry5.util.TextStreamResponse;
 
 
 @Import( stylesheet="context:/css/littlehelper.css" )
 public class Index {
+	
+	@Inject
+	private HttpServletRequest request;
+	
+	public Object onActionFromMyIp() {
+		
+		return new TextStreamResponse( "text/plain", request.getRemoteAddr() );
+	}
 }
