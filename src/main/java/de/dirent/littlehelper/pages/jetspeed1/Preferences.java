8e1e1abd440e143b6250c6d7beaa8569aca38d16
dirk <dirk@0d842271-ca0e-0410-b7f0-83f617a2c6fd>
2011-08-23 20:06:25 +0000

    Jetspeed 1 History: Preferences
    git-svn-id: http://wikiserver/svn/littlehelper/trunk@684 0d842271-ca0e-0410-b7f0-83f617a2c6fd

---
+++ src/main/java/de/dirent/littlehelper/pages/jetspeed1/Preferences.java
@@ -0,0 +1,9 @@
+package de.dirent.littlehelper.pages.jetspeed1;
+
+
+import org.apache.tapestry5.annotations.Import;
+
+
+@Import( stylesheet = { "context:/css/littlehelper.css", "context:/css/jetspeed1.css" } )
+public class Preferences {
+}
