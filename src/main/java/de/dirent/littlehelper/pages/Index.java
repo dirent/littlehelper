f947ae701d20771df5d3886e9fd3e7d907b86645
dirk <dirk@0d842271-ca0e-0410-b7f0-83f617a2c6fd>
2013-09-30 16:31:20 +0000

    Beispiel für Cache Manifest
    git-svn-id: http://wikiserver/svn/littlehelper/trunk@914 0d842271-ca0e-0410-b7f0-83f617a2c6fd

--- src/main/java/de/dirent/littlehelper/pages/Index.java
+++ src/main/java/de/dirent/littlehelper/pages/Index.java
@@ -1,10 +1,17 @@
 package de.dirent.littlehelper.pages;
 
 
+import java.io.ByteArrayInputStream;
+import java.io.IOException;
+import java.io.InputStream;
+
 import javax.servlet.http.HttpServletRequest;
 
+import org.apache.tapestry5.StreamResponse;
 import org.apache.tapestry5.annotations.Import;
+import org.apache.tapestry5.annotations.OnEvent;
 import org.apache.tapestry5.ioc.annotations.Inject;
+import org.apache.tapestry5.services.Response;
 import org.apache.tapestry5.util.TextStreamResponse;
 
 
@@ -17,5 +24,33 @@ public class Index {
 	public Object onActionFromMyIp() {
 		
 		return new TextStreamResponse( "text/plain", request.getRemoteAddr() );
+	}
+
+
+	public Object onActionFromManifest() {
+		
+		return new StreamResponse() {
+			
+			@Override
+			public void prepareResponse(Response response) {
+
+
+				response.setHeader( "Expires", "0" );
+			}
+			
+			@Override
+			public InputStream getStream() throws IOException {
+
+				String manifest = "CACHE MANIFEST\nhtml5app.html\nhtml5app.js";
+				
+				return new ByteArrayInputStream( manifest.getBytes( "utf-8" ) );
+			}
+			
+			@Override
+			public String getContentType() {
+
+				return "text/cache-manifest";
+			}
+		};
 	}
 }
