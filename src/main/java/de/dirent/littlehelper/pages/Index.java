f92e9bb8af2514cc991ca3be6dbed686a40e10bc
dirk <dirk@0d842271-ca0e-0410-b7f0-83f617a2c6fd>
2013-10-05 12:17:44 +0000

    Beispiel für Test auf Zweierpotenz
    git-svn-id: http://wikiserver/svn/littlehelper/trunk@915 0d842271-ca0e-0410-b7f0-83f617a2c6fd

--- src/main/java/de/dirent/littlehelper/pages/Index.java
+++ src/main/java/de/dirent/littlehelper/pages/Index.java
@@ -8,9 +8,13 @@ import java.io.InputStream;
 import javax.servlet.http.HttpServletRequest;
 
 import org.apache.tapestry5.StreamResponse;
+import org.apache.tapestry5.annotations.Component;
 import org.apache.tapestry5.annotations.Import;
-import org.apache.tapestry5.annotations.OnEvent;
+import org.apache.tapestry5.annotations.Persist;
+import org.apache.tapestry5.annotations.Property;
+import org.apache.tapestry5.corelib.components.Zone;
 import org.apache.tapestry5.ioc.annotations.Inject;
+import org.apache.tapestry5.services.Request;
 import org.apache.tapestry5.services.Response;
 import org.apache.tapestry5.util.TextStreamResponse;
 
@@ -18,12 +22,50 @@ import org.apache.tapestry5.util.TextStreamResponse;
 @Import( stylesheet="context:/css/littlehelper.css" )
 public class Index {
 	
+	@Property @Persist
+	private Integer input;
+	
+	@Property
+	private String inputMessage;
+	
+	@Component
+	private Zone inputZone;
+	
 	@Inject
-	private HttpServletRequest request;
+	private Request request;
+	
+	public Object onSuccessFromInput() {
+	
+		int potenz = 1;
+		boolean isPowOfTwo = input == potenz;
+		
+		while( !isPowOfTwo  &&  potenz < input ) {
+			
+			potenz *= 2;
+			isPowOfTwo = input == potenz;
+		}
+		
+		if( isPowOfTwo ) {
+			
+			this.inputMessage = "Die Eingabe ist eine Zweiterpotenz.";
+			
+		} else {
+			
+			this.inputMessage = "Die Eingabe ist keine Zweiterpotenz.";
+		}
+		
+		if( request.isXHR() ) return inputZone;
+		
+		return null;
+	}
+	
+	
+	@Inject
+	private HttpServletRequest httpRequest;
 	
 	public Object onActionFromMyIp() {
 		
-		return new TextStreamResponse( "text/plain", request.getRemoteAddr() );
+		return new TextStreamResponse( "text/plain", httpRequest.getRemoteAddr() );
 	}
 
 
